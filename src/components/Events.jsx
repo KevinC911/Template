d48a3be4490c01f5c295fb9edf388792704693b0
KevinC911 <00157320@uca.edu.sv>
2023-05-22 11:56:37 -0600

    some fixes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,15 +1,20 @@
 function Events() {
 
+    function formatDate (string) {
+        var options = {month: 'long', day: 'numeric' };
+        return new Date(string).toLocaleDateString([],options);
+    }
+
     const eventsData = [
         {
-            title: "Gragas Wild Rift",
+            title: "Gragas Panzón",
             image: "https://static1-es.millenium.gg/articles/2/34/01/2/@/157793-kda-gragas-article_cover_bd-1.jpg",
-            date: "24/07/2024",
+            date: "2018-05-18T04:00:00.000Z",
             time: "120",
         },
         {
-            title: "Marin Kitagawa",
-            image: "https://somoskudasai.com/wp-content/uploads/2023/01/portada_sono-bisque-doll-130.jpg",
+            title: "Convencion de Otakus y personas sin oficio",
+            image: "https://ichef.bbci.co.uk/news/624/mcs/media/images/81966000/jpg/_81966848_otakuchracters.jpg",
             date: "24/07/2024",
             time: "120",
         }
@@ -18,10 +23,10 @@ function Events() {
         <div className="flex flex-row w-screen h-24 font-rubik">
             {eventsData.map((data) => {
                 return (
-                    <section className="transition duration-400 shadow-lg ml-2 mr-2 rounded-sm flex flex-col p-2 h-fit w-96 items-center bg-gray-200 hover:shadow-blue-200">
+                    <section className="transition duration-400 shadow-lg ml-2 mr-2 rounded-sm flex flex-col p-2 h-fit w-96 bg-gray-200 hover:shadow-blue-200">
                         <img src={data.image}/>
                         <b>{data.title}</b>
-                        <h2>{data.date}</h2>
+                        <h2>{formatDate(data.date)}</h2>
                         <h2>{data.time} min</h2>
                     </section>
                 )
